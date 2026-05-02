c8ab1d531bd0814ab92df69bd3583afa20445871
AndreNTeixeira <andrenogueirab@gmail.com>
2026-05-02 11:02:32 -0300

    Use native click() with presence check instead of JS click

--- web/pages/base_page.py
+++ web/pages/base_page.py
@@ -14,7 +14,8 @@ class BasePage:
 
     def click(self, locator):
         element = self.wait.until(EC.presence_of_element_located(locator))
-        self.driver.execute_script("arguments[0].click();", element)
+        element.click()
+
 
 
 
