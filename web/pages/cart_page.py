4f8d1757a6c5bc251ddded4d5c658fab5367f8df
AndreNTeixeira <andrenogueirab@gmail.com>
2026-05-02 11:13:25 -0300

    Wait for checkout page URL after clicking checkout button

--- web/pages/cart_page.py
+++ web/pages/cart_page.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from .base_page import BasePage
 
 CART_ITEM_NAME = (By.CLASS_NAME, "inventory_item_name")
@@ -7,6 +8,6 @@ BTN_CHECKOUT   = (By.ID, "checkout")
 class CartPage(BasePage):
     def get_item_name(self) -> str:
         return self.get_text(CART_ITEM_NAME)
-
     def proceed_to_checkout(self):
         self.click(BTN_CHECKOUT)
+        self.wait.until(EC.url_contains("checkout-step-one"))
