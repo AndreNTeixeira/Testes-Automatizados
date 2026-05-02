63050a33ae1824361b33867f328ccc368c313c7a
AndreNTeixeira <andrenogueirab@gmail.com>
2026-05-02 10:48:26 -0300

    Set window size for headless Chrome in CI

--- web/conftest.py
+++ web/conftest.py
@@ -8,6 +8,7 @@ def driver():
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1080")
     browser = webdriver.Chrome(options=options)
     browser.implicitly_wait(5)
     yield browser
