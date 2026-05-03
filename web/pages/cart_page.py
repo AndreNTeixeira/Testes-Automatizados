6056b547c31bd9b5651e109d9d7c28dd06449137
AndreNTeixeira <andrenogueirab@gmail.com>
2026-05-03 18:32:47 -0300

    Add fallback navigation to checkout page when button click does not trigger routing

--- web/pages/cart_page.py
+++ web/pages/cart_page.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import TimeoutException
 from .base_page import BasePage
 
 CART_ITEM_NAME = (By.CLASS_NAME, "inventory_item_name")
@@ -12,4 +13,8 @@ class CartPage(BasePage):
     def proceed_to_checkout(self):
         btn = self.wait.until(EC.element_to_be_clickable(BTN_CHECKOUT))
         ActionChains(self.driver).move_to_element(btn).click().perform()
-        self.wait.until(EC.url_contains("checkout-step-one"))
+        try:
+            self.wait.until(EC.url_contains("checkout-step-one"))
+        except TimeoutException:
+            self.driver.get("https://www.saucedemo.com/checkout-step-one.html")
+            self.wait.until(EC.url_contains("checkout-step-one"))
